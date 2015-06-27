52ea413a9db7e7c98c1a1047c8b085eb2d117fc8
Ben Batschelet <ben@codeup.com>
2015-06-27 12:44:50 -0500

    Handle cache false positives

--- app/Http/Controllers/S3Controller.php
+++ app/Http/Controllers/S3Controller.php
@@ -29,9 +29,30 @@ class S3Controller extends Controller
             return $response;
         }
 
-        $content     = $this->fs->read($path);
+        $content = $this->fs->read($path);
+
+        if (!$content) {
+            // We had a false positive from the cache!!
+            $this->deleteFromCache($path);
+
+            abort(404);
+        }
+
         $contentType = $this->fs->getMimetype($path);
 
         return $response->header('Content-Type', $contentType)->setContent($content);
+    }
+
+    protected function deleteFromCache($path)
+    {
+        // Feels a bit more complex than it should be, but at
+        // least we're not having to reverse engineer the cache
+
+        $factory = app('flysystem.cachefactory');
+        $config  = config('flysystem.cache.default');
+
+        $cache = $factory->make($config, $this->fs);
+
+        $cache->storeMiss($path);
     }
 }
